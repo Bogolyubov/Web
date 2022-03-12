a2593ad98ff8755e16017f4246ddb4b9b870ea58
Bogolyubov <79753802+Bogolyubov@users.noreply.github.com>
2022-03-12 19:01:28 +0500

    LR1

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<p> Дата и время:
+<p>
+<?php
+ $d=date("d.m.Y H:i");
+ echo($d);
+
+
+
+
+?>
+
+<p> Ссылки на Лабораторную работу №1:
+<p>
+<ul>
+<li><a href="Lab1.html">Задание 1</a></li>
+<li><a href="Lab2.html">Задание 2</a></li>
+<li><a href="Lab3.html">Задание 3</a></li>
+<li><a href="Lab4.html">Задание 4</a></li>
+<li><a href="Lab5.html">Задание 5</a></li>
+<li><a href="Lab6.html">Задание 6</a></li>
+<li><a href="Lab7.html">Задание 7</a></li>
+
+
+
+</ul>
---
+++ main.js
@@ -0,0 +1,5 @@
+function aler(){
+
+    alert ("Добрый день") ;
+
+    }
